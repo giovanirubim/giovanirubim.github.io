5644a1c23f90992f8e28090c376071ec198cf53c
Giovani Rubim Francisco <giovanirubim.f@gmail.com>
2021-01-25 13:14:25 -0300

    pwd-encryptor

---
+++ pwd-encryptor/index.js
@@ -0,0 +1,119 @@
+import {encrypt, decrypt} from './encryption.js'
+let key = ''
+const dom = {}
+const centralize = () => {
+	const sx = dom.main.css('width').replace('px', '')*1
+	const sy = dom.main.css('height').replace('px', '')*1
+	const full_sx = window.innerWidth
+	const full_sy = window.innerHeight
+	dom.main.css({
+		'margin-top': Math.floor((full_sy - sy)*0.4) + 'px'
+	})
+}
+const hide = () => new Promise((done) => {
+	if (!dom.text.text()) {
+		dom.main.css({ opacity: 0 })
+		return done()
+	}
+	dom.main.animate({ opacity: 0 }, 500, () => {
+		dom.text.html('')
+		dom.content.html('')
+		done()
+	})
+})
+const show = () => new Promise((done) => {
+	centralize()
+	dom.main.animate({ opacity: 1 }, 500, done)
+	dom.main.find('input[type="text"]').first().focus()
+})
+const addInput = () => {
+	dom.content.append(`
+		<div>
+			<input type="text">
+		</div>
+	`)
+	return dom.content.find('input').last()
+}
+const addButton = (text) => {
+	dom.content.append(`
+		<div>
+			<input type="button" value="${text}">
+		</div>
+	`)
+	return dom.content.find('input').last()
+}
+const addTextblock = (content) => {
+	dom.content.append(`
+		<div>
+			<textarea disabled></textarea>
+		</div>
+	`)
+	const res = dom.content.find('textarea').last()
+	res.val(content)
+	return res
+}
+const options = async () => {
+	await hide()
+	dom.text.text('O que deseja fazer?')
+	addButton('Salvar uma senha').on('click', store)
+	addButton('Recuperar senha de um código').on('click', recover)
+	addButton('Criar uma senha segura')
+	show()
+}
+const showCode = async (code) => {
+	await hide()
+	dom.text.text('Código gerado. Salve este código para poder recuperar a senha se precisar')
+	addTextblock().val(code)
+	addButton('Voltar').on('click', options)
+	show()
+}
+const showPass = async(pass) => {
+	await hide()
+	dom.text.text('Senha recuperada')
+	addTextblock().val(pass)
+	addButton('Voltar').on('click', options)
+	show()
+}
+const recover = async() => {
+	await hide()
+	dom.text.text('Insira o código salvo da senha')
+	let input = addInput()
+	addButton('Continuar').on('click', () => {
+		showPass(decrypt(key, input.val()))
+	})
+	addButton('Voltar').on('click', options)
+	show()
+}
+const store = async () => {
+	await hide()
+	dom.text.text('Insira a senha que deseja salvar')
+	let input = addInput()
+	addButton('Continuar').on('click', () => {
+		showCode(encrypt(key, input.val()))
+	})
+	addButton('Voltar').on('click', options)
+	show()
+}
+const start = async () => {
+	await hide()
+	dom.text.text('Para começarmos insira a senha mestre')
+	let input = addInput()
+	addButton('Continuar').on('click', () => {
+		key = input.val()
+		options()
+	})
+	show()
+}
+$(document).ready(() => {
+	$('form').on('submit', e => {
+		const target = $('input[type="button"]:focus')
+		target.trigger('click')
+		return false
+	})
+	dom.main = $('.main')
+	dom.text = $('.text')
+	dom.content = $('.content')
+	centralize()
+	$(window).on('resize', centralize)
+	start()
+})
---
+++ pwd-encryptor/pwd-generator.js
@@ -0,0 +1,46 @@
+const range = (a, b) => {
+	a = a.charCodeAt(0)
+	b = b.charCodeAt(0)
+	return new Array(a - b + 1).fill().map((_,i) => String.fromCharCode(a + i))
+}
+const alphabets = {
+	uppercase: range('A', 'Z'),
+	lowercase: range('a', 'z'),
+	digits: range('0', '9'),
+	specialChars: '!#$%&()*+,--./:;=?@[]{}'.split('')
+}
+const toAlphabet = {}
+for (let name in alphabets) {
+	const alphabet = alphabets[name]
+	alphabet.forEach(char => toAlphabet[char] = alphabet)
+}
+const getAlphabets = (template) => {
+	const array = []
+	for (let char of template) {
+		array.push(toAlphabet[char])
+	}
+	return array
+}
+const pick = (array) => array[Math.floor(Math.random()*array.length)]
+const shuffle = (array) => {
+	for (let i=array.length; i>1;) {
+		const j = Math.floor(Math.random()*(i--))
+		const aux = array[j]
+		array[j] = array[i]
+		array[i] = aux
+	}
+	return array
+}
+export default (size, template = 'aA0?') => {
+	let res = []
+	let all = []
+	for (alphabet of alphabets) {
+		res.push(pick(alphabet))
+		all.push(...alphabet)
+	}
+	while (res.length < size) {
+		res.push(pick(all))
+	}
+	shuffle(res)
+	return res.join('')
+}
