e4e83e9bd9df96355b2b4868ea40cafd4dcd0588
giovanirubim <giovanirubim.f@gmail.com>
2022-01-30 16:14:22 -0300

    Degree grid

--- trilateration/sphere/shaders.js
+++ trilateration/sphere/shaders.js
@@ -31,8 +31,8 @@ const earthFrag = `
 		return min(1.0, max(0.0, a));
 	}
 
-	float sqrValue(float val) {
-		float range = 0.005;
+	float gridValue(float val, float div, float range) {
+		val = mod(val, 1.0/div)*div;
 		float a = (range - val)/range*2.0;
 		float b = (val - (1.0 - range))/range*2.0;
 		return oneInterval(oneInterval(a) + oneInterval(b));
@@ -40,18 +40,22 @@ const earthFrag = `
 
 	void main(void)
 	{
-		vec4 map = texture2D(earth, vUv);
+		vec4 map = texture2D(earth, vec2(mod(vUv.x + 0.25, 1.0), vUv.y));
 		float img_x = 1.0 - vUv.x;
 		img_x = mod(img_x + 0.00067 - ariesGHA/6.283185307179586 + 1.0, 1.0);
 		float img_y = vUv.y;
 		vec4 star = texture2D(stars, vec2(img_x, img_y));
-		float latDiv = 18.0;
-		float longDiv = 36.0;
-		float latSqr = sqrValue(mod(vUv.x, 1.0/longDiv)*longDiv);
-		float longSqr = sqrValue(mod(vUv.y, 1.0/latDiv)*latDiv);
+		float mainGrid =
+			gridValue(vUv.x, 36.0, 0.005) +
+			gridValue(vUv.y, 18.0, 0.005);
+		float minGrid = 
+			gridValue(vUv.x, 360.0, 0.01) +
+			gridValue(vUv.y, 180.0, 0.01);
 		float bright = 0.0;
-		bright += oneInterval(latSqr + longSqr)*0.25;
-		bright += (star.r + star.g + star.b)/3.0;
+		bright +=
+			oneInterval(mainGrid)*0.25 +
+			oneInterval(minGrid)*0.1;
+		bright += pow((star.r + star.g + star.b)/3.0, 1.5);
 		vec3 c = map.rgb + vec3(bright);
 		gl_FragColor = vec4(c, 1.0);
 	}
