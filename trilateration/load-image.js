2f036d272cbced39ab6f0bfcddcc5c5fc6bc80e0
giovanirubim <giovanirubim.f@gmail.com>
2022-02-14 00:30:59 -0300

    onlinebacon clear

--- trilateration/load-image.js
+++
@@ -1,8 +0,0 @@
-const loadImage = src => new Promise((done, fail) => {
-	const image = new Image();
-	image.onload = () => done(image);
-	image.onerror = error => fail(error);
-	image.src = src;
-});
-
-export default loadImage;
